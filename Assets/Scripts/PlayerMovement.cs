03598281540e3df8c7c31350c1c6d4a36b3e8632
VerifyingUsername <lkyben6@gmail.com>
2022-01-21 17:35:52 +0800

    PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    Rigidbody PlayerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        PlayerRigidBody.AddForce(movement * speed * Time.deltaTime);
     }
 }
